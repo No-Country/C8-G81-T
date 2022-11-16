8dc49bbfa02597346a465f6a5b5f3faaf78c1a74
maalroba22 <maalroba22@gmail.com>
2022-11-16 06:31:37 -0500

    feat:add-prisma-cors and setting

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
+const { json } = require('express')
 const express = require('express')
-const app = express()
-const port = process.env.PORT || 3001
+const cors = require('cors')
 
+
+const app = express()
+app.use(json())
+app.use(cors())
+
+
+
+const port = process.env.PORT || 3001
 const routes = require('./src/routes/')
 
 routes(app)
