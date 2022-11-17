cd651c477b8d7784cc8956976f968038c4471005
David Castro <david.castroanaya@gmail.com>
2022-11-16 19:49:47 -0600

    feat: Add new routes

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ function App() {
         <Route path='/profile' element={<ProfilePage/>}/>
         <Route path='/login' element={<div>Login</div>}/>
         <Route path='/turn' element={<div>Turn</div>}/>
+        <Route path='/register' element={<div>Register</div>}/>
+        <Route path='/patients' element={<div>Patients</div>}/>
       </Routes>
     </div>
   );
