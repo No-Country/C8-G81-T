133a06f63b1c896dc62aff80adb6f2a0a1d1db93
Francisco Daniel Molina <77471857+TheFranciscoMolina@users.noreply.github.com>
2022-11-18 23:05:34 -0300

    feat: add register to app

--- api/src/controllers/doctors.js
+++ api/src/controllers/doctors.js
@@ -1,39 +1,28 @@
-const prisma = require('../db/index')
-
+const prisma = require("../db/index");
 
 exports.getAllDoctors = async (req, res) => {
-    try {
-        const allDoctors = await prisma.doctors.findFirst({})
-        res.send(allDoctors)
-    } catch (error) {
-        res.send(error)
-    }
-}
+  try {
+    const allDoctors = await prisma.doctors.findMany({});
+    res.send(allDoctors);
+  } catch (error) {
+    res.send(error);
+  }
+};
 
+exports.addDoctors = async (req, res) => {
+  const { dni, firstname, lastname, specialty, email, password, phone } =
+    req.body;
 
-exports.addDoctors = async (req,res) => {
-    const {
-        dni,
-        firstname,
-        lastname,
-        specialty,
-        email,
-        password,
-        phone
-    } = req.body
-
-const addDoctor = await prisma.doctors.create({
-    data:{
-        dni,
-        firstname,
-        lastname,
-        specialty,
-        email,
-        password,
-        phone 
-    }
-})
-res.send({msg:'add doctors Susccess'})
-
-
-}
+  const addDoctor = await prisma.doctors.create({
+    data: {
+      dni,
+      firstname,
+      lastname,
+      specialty,
+      email,
+      password,
+      phone,
+    },
+  });
+  res.send({ msg: "add doctors Susccess" });
+};
--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 const Login = React.lazy(() => import("./components/Login/Login"));
 const DefaultLayout = React.lazy(() => import("./layout/DefaultLayout"));
 const Home = React.lazy(() => import("./views/Home/"));
+const Register = React.lazy(() => import("./views/Register"));
 const ProfilePage = React.lazy(() => import("./views/Profile"));
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<DefaultLayout />}>
             <Route index element={<Home />} />
-            <Route path="register" element={<div>Register</div>} />
+            <Route path="register" element={<Register />} />
             <Route path="profile" element={<ProfilePage />} />
             <Route path="login" element={<Login />} />
             <Route path="turn" element={<div>Turn</div>} />
