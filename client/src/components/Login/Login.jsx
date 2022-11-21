8147363197d7c0421f5200288d66e1cf83caef91
tineocac <carlostineocac@gmail.com>
2022-11-21 12:30:38 -0400

    update: change base of login component

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,60 +1,31 @@
-import { Formik, Field, Form, ErrorMessage } from "formik";
-import axios from 'axios'
+import '../../styles/Login.css'
 
 const Login = () => {
 
-  const submit = (data) => {
-    axios
-      .post('http://localhost:3001/ruta-login-por-crear', data)
-      .then(res => {
-        localStorage.setItem('token', res.token)
-
-      })
-      .catch(error => {
-        if (error.response.status === 404) {
-          alert('wrong credentials')
-        }
-        console.log(error. response);
-      })
-  }
-
   return (
-    <div>
-      <h3>Login</h3>
-      <Formik
-        onSubmit={submit}
-      >
-        {({ errors, touched, isSubmitting }) => (
-          <Form>
-            <label htmlFor="email">Email:</label>
-            <Field
-              id="email"
-              name="email"
-              placeholder="example@mail.com"
-              type="email"
-            />
-            {errors.email && touched.email && (
-              <ErrorMessage component="div" name="email" />
-            )}
-            <label htmlFor="password">Contraseña:</label>
-            <Field
-              id="password"
-              name="password"
-              type="password"
-              placeholder="*****"
-            />
-            {errors.password && touched.password && (
-              <ErrorMessage component="div" name="password" />
-            )}
-            <button type="submit">Ingresar</button>
-            {isSubmitting ? (
-              <div>
-                <p>Enviando credenciales...</p>
-              </div>
-            ) : null}
-          </Form>
-        )}
-      </Formik>
+    <div className="login">
+      <div className="form">
+        <form noValidate>
+          <span>Login</span>
+
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter email id / username"
+            className="form-control inp_text"
+            id="email"
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            className="form-control"
+          />
+
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 };
