8eef1620c5abbf7b47a4080321244268d9d83e05
maalroba22 <maalroba22@gmail.com>
2022-11-21 22:50:55 -0500

    add login Doctors

---
+++ api/src/controllers/login.js
@@ -0,0 +1,43 @@
+const prisma = require('../db/')
+
+exports.loginDoctors = (async (req, res) => {
+    const { email, password } = req.body
+  
+    try {
+
+        if (!email || !password) {
+            res.status(400).send({ msg: 'Usuario y contraseña son Requeridos' })
+            return
+        }
+        const emailExiste = await prisma.doctors.findUnique({
+            where: {
+                email: email
+            }
+        })
+        
+        if(!emailExiste){
+            res.status(400).send({ msg: 'Email no  Existe ' })
+            return
+        }else {
+            const passworerror = emailExiste.password
+            if (passworerror !=password) {
+                res.status(400).send({ msg: 'passwor incorrecto  ' })
+                return
+            }else {
+                res.status(200).send({ msg: 'ingreso al Sistema ok ' })
+            }
+        }
+        
+
+           
+
+    } catch (error) {
+
+    }
+
+
+})
+exports.loginPatiens = (async (req, res) => {
+
+    res.send({ msg: 'exito logueado pacientes' })
+})
